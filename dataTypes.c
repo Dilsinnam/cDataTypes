9f5912a2a43841e93d55052d20f8f85c0ab4535a
Dilsin Amirtharajah <DilsinAmirtharajahUSA@gmail.com>
2023-09-14 14:29:44 -0400

    Initial Commit

---
+++ dataTypes.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age = 29;                              /*Integer is always a number, no "" needed*/
+    double gpa = 7.5;                          /*Double for decimals, can use others like "3.0"*/
+    char grade = 'Q';                          /*Can only store ONE character, under single qotation marks ('')*/
+    char school[] = "Super Senior University"; /*Phrase given in "" is what shows up in brackets []*/
+
+    return 0;
+}
